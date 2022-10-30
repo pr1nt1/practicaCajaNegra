a79f24ed6cfb602ebf302be21bc449b017ac196b
laurargomez <laura.rgomez@alumnos.upm.es>
2022-10-30 19:17:51 +0100

    update proyect

---
+++ src/main/java/com/practica/cajanegra/Pruebas.java
@@ -0,0 +1,8 @@
+package com.practica.cajanegra;
+import com.binarytree.BinaryTree;
+public class Pruebas {
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree("1");
+        System.out.println(bt);
+    }
+}
